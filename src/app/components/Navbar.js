8acb6dec324d9b7909aebe2c5ebd4577b4602118
AdityaSingh772 <workwithracian@gmail.com>
2024-08-31 03:24:22 +0530

    wallet connection done

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,8 +1,70 @@
-// components/Navbar.js
+"use client";
 import React from 'react';
 import Link from 'next/link';
 
+import {ethers} from 'ethers';
+import { useState } from 'react';
+ 
+
+const networks = {
+    polygon: {
+        chainId: `0x${Number(80002).toString(16)}`,
+        chainName: "Amoy",
+        nativeCurrency: {
+            name: "MATIC",
+            symbol: "MATIC",
+            decimals: 18
+        },
+        rpcUrls: ["https://rpc-amoy.polygon.technology/"],
+        blockExplorerUrls: [" https://amoy.polygonscan.com/"]
+    },
+};
+
+
+
+
 export default function Navbar() {
+
+  const [address, setAddress] = useState("connect Wallet");
+  const [balance, setBalance] = useState("-");
+
+    const connectWallet = async() =>{
+        await window.ethereum.request({method: "eth_requestAccounts" });
+        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+        if(provider.network !== "matic") {
+          await window.ethereum.request(
+            {
+              method : "wallet_addEthereumChain",
+              params: [
+                {
+                  ...networks["polygon"]
+                }
+              ]
+            }
+          )
+
+
+          const account = provider.getSigner();
+          const Address = await account.getAddress();
+          setAddress(Address);
+
+          const Balance = ethers.utils.formatEther( await account.getBalance());
+          setBalance(Balance);
+        }
+        else{
+          const account = provider.getSigner();
+          const Address = await account.getAddress();
+          setAddress(Address);
+
+          const Balance = ethers.utils.formatEther( await account.getBalance());
+          setBalance(Balance);
+        }
+
+        
+    }
+
+
   return (
     <header className="bg-blue-950 shadow-md py-4 px-8">
       <div className="container mx-auto flex justify-between items-center">
@@ -18,9 +80,9 @@ export default function Navbar() {
           <Link href="/create" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
           Create Campaign
           </Link>
-          <Link href="/connect" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-          Connect Wallet
-          </Link>
+          <div onClick={connectWallet} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          {balance.slice(0,5)}MATIC  {address.slice(0,7)}...{address.slice(39)}
+          </div>
         </div>
       </div>
     </header>
