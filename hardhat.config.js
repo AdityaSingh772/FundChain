52bb8c69f38327a30c9f9a7f9dc9c08f2336f5d4
AdityaSingh772 <workwithracian@gmail.com>
2024-08-31 02:28:10 +0530

    conntracts added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,25 @@
-import { task } from "hardhat/config"; // Example import
+const { task } = require("hardhat/config");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({ path: './.env.local'});
 
-export default {
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY; 
+
+module.exports = {
   solidity: "0.8.10",
-  defaultNetwork: "hardhat",
+  defaultNetwork: "polygon",
   networks: {
     hardhat: {},
+    polygon: {
+      url : process.env.NEXT_PUBLIC_RPC_URL,
+      accounts : [privateKey],
+    },
   },
 };
