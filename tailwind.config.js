2ffff7ed093259dd9ae39fe687dbb3345aa0c49f
Dinesh Jangid <spideyyyyy89@gmail.com>
2024-08-31 02:23:03 +0530

    added Navar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        b1: "#77C8BF",
+        b2: "#C8E2E1",
+        b3: "#B3C8CF",
+      },
+      fontFamily:{
+        custom: ['CustomFont', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
     },
   },
   plugins: [],
