2e6dd5e5c378541906c562cb45e93bba75f2a960
Aditya Singh <112646772+AdityaSingh772@users.noreply.github.com>
2024-08-30 23:53:40 +0530

    Revert "Step-1 -->Smart Contracts Sorted"

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,9 @@
-// Import required dependencies
-require("@nomiclabs/hardhat-waffle");
-require('dotenv').config({ path: './.env.local' });
+import { task } from "hardhat/config"; // Example import
 
-// Define a custom task to print the list of accounts
-task("accounts", "Print the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// Get the private key from environment variables
-const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24",
-  defaultNetwork: "polygon",
+export default {
+  solidity: "0.8.10",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
-    polygon: {
-      url: process.env.NEXT_PUBLIC_RPC_URL,
-      accounts: [privateKey]
-    }
-  }
+  },
 };
